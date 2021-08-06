913f9cfc3021628fdf271f40e31ae8b1971d17ae
GeonjinLee <geonjinlee712@gmail.com>
2021-08-07 03:53:11 +0900

    cook

---
+++ Python/triangle.py
@@ -0,0 +1,11 @@
+def star(s):
+    for i in range(s):
+        print('*' * ( i + 1))
+
+star(int(input()))
+
+def star1(s):
+    for i in range(s):
+        print('&' * (i + 1))
+
+star1(int(input()))
